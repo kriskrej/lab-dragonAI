095980fe8339975152b865b6f9e1df607109ab0c
Kris Krej <kris@krej.net>
2018-03-27 09:15:25 +0200

    Project - first version

---
+++ Assets/DragonAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class DragonAI : MonoBehaviour {
+    GameObject player;
+    DragonAnimator dragonAnimator;
+
+	void Start () {
+	    player = GameObject.FindGameObjectWithTag("Player");
+	    dragonAnimator = GetComponent<DragonAnimator>();
+	}
+	
+	void Update () {
+	    if (Vector3.Distance(transform.position, player.transform.position) < 5f) {
+	        dragonAnimator.PlayAttackAnimation();
+            Invoke("RestartLevel", 1);
+        }
+	}
+
+
+    void RestartLevel() {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
